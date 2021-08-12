0d0ee2e35dd446a7f433502052dddc070ae77f83
Dhailyn <85664739+Dhailyn@users.noreply.github.com>
2021-08-12 11:35:36 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("-----****Hello World & Welcome to Jenkins Training****-----");
+        System.out.println("-----**** Made Some Changes****-----");
 
     }
 }
